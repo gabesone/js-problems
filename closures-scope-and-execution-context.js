aeea83e819feb02904fdcb0fb0114519230e1349
gabesone <sena6.gabriel@gmail.com>
2024-12-06 16:09:00 -0300

    challenge 7 completed

--- closures-scope-and-execution-context.js
+++ closures-scope-and-execution-context.js
@@ -116,14 +116,24 @@ function delay(func, wait) {
 // delay(testDelay, 1000);
 
 // CHALLENGE 7
-function rollCall(names) {}
+function rollCall(names) {
+  let counter = 0;
+  return function () {
+    if (counter == names.length) {
+      console.log("Everyone accounted for");
+    } else {
+      console.log(names[counter]);
+      counter++;
+    }
+  };
+}
 
 // /*** Uncomment these to check your work! ***/
-// const rollCaller = rollCall(['Victoria', 'Juan', 'Ruth'])
-// rollCaller() // => should log 'Victoria'
-// rollCaller() // => should log 'Juan'
-// rollCaller() // => should log 'Ruth'
-// rollCaller() // => should log 'Everyone accounted for'
+const rollCaller = rollCall(["Victoria", "Juan", "Ruth"]);
+rollCaller(); // => should log 'Victoria'
+rollCaller(); // => should log 'Juan'
+rollCaller(); // => should log 'Ruth'
+rollCaller(); // => should log 'Everyone accounted for'
 
 // CHALLENGE 8
 function saveOutput(func, magicWord) {}
